266ccacc311c198b08801f71ad00e6ec77a9102a
Gleb G <mrp4p3r@gmail.com>
2018-01-05 17:41:02 +0300

    [REFACTOR] split restart function into restart many and restart

--- src/commands/restart.go
+++ src/commands/restart.go
@@ -4,40 +4,46 @@ import (
 	"log"
 	"os/exec"
 	"github.com/jawher/mow.cli"
-	"fmt"
 )
 
 func RestartCmdEntry(cmd *cli.Cmd) {
 	cmd.Spec = "SERVICES..."
 	servicesList := cmd.StringsArg("SERVICES", nil, "Services to restart")
 	cmd.Action = func() {
-		restart(servicesList)
+		restartMany(servicesList)
 	}
 }
 
-func restart(servicesList *[]string) {
+func restartMany(servicesList *[]string) {
 	for idx := range *servicesList {
 		serviceName := (*servicesList)[idx]
-		proc := exec.Command("immortalctl", "restart", serviceName)
-
-		err := proc.Start()
-		if err != nil {
-			log.Println("Could not execute immortalctl")
-			log.Println(err.Error())
-			restartErrMsg(serviceName)
-			continue
+		err := restart(serviceName)
+		if err == nil {
+			log.Printf("Send restart command for %s. OK\n", serviceName)
 		}
-
-		err = proc.Wait()
-		if err != nil {
-			log.Println("Failed to wait until restart process is done")
-			log.Println(err.Error())
-			restartErrMsg(serviceName)
-			continue
-		}
-
-		fmt.Printf("Send restart command for %s. OK\n", serviceName)
 	}
+}
+
+func restart(serviceName string) error {
+	proc := exec.Command("immortalctl", "restart", serviceName)
+
+	err := proc.Start()
+	if err != nil {
+		log.Println("Could not execute immortalctl")
+		log.Println(err.Error())
+		restartErrMsg(serviceName)
+		return err
+	}
+
+	err = proc.Wait()
+	if err != nil {
+		log.Println("Failed to wait until restart process is done")
+		log.Println(err.Error())
+		restartErrMsg(serviceName)
+		return err
+	}
+
+	return nil
 }
 
 func restartErrMsg(serviceName string) {
